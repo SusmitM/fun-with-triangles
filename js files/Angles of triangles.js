e53df74745bb953ca9e523dddeb13e8996ba9868
Susmit Mukherjee <susmitmukherjee2001@gmail.com>
2022-02-07 15:31:21 +0530

    feature: added second page

--- js files/Angles of triangles.js
+++ js files/Angles of triangles.js
@@ -2,25 +2,25 @@ var Angle1 = parseInt(document.querySelector("#angle1").value);
 var Angle2 = parseInt(document.querySelector("#angle2").value);
 var Angle3 = parseInt(document.querySelector("#angle3").value);
 var output = document.querySelector("#output");
-const btnCheck= document.querySelector("#btnCheck");
+const btnCheck = document.querySelector("#btnCheck");
 
-function sumCalculator(){
-    var result =Angle1+Angle2+Angle3;
+function sumCalculator() {
+    var result = Angle1 + Angle2 + Angle3;
     return result;
 }
-function clickHandler(){
+
+function clickHandler() {
     var sum = sumCalculator(Angle1, Angle2, Angle3);
     console.log(sum);
-    if(sum===180){
-        output.innerText="Yay, the angles form a triangle!";
-        
+    if (sum === 180) {
+        output.innerText = "Yay, the angles form a triangle!";
+
+
+    } else {
+        output.innerText = "Oh Oh! The angle doesn't form a triangle";
+
 
     }
-    else{
-        output.innerText="Oh Oh! The angle doesn't form a triangle";
-       
 
-    }
-  
 }
 btnCheck.addEventListener("click", clickHandler);
---
+++ js files/Calculatehypotenuse.js
@@ -0,0 +1,16 @@
+const baseVal = parseInt(document.querySelector("#baseValue").value);
+const heightVal = parseInt(document.querySelector("#heightValue").value);
+const btnCheck = document.querySelector("#btnCheck");
+
+function hypotenuseCal(baseVal, heightVal) {
+
+}
+
+function clickHandler() {
+
+    var result = Math.sqrt(baseVal * baseVal + heightVal * heightVal);
+
+    output.innerText = "The length of hypotenuse is" + " " + result;
+
+}
+btnCheck.addEventListener("click", clickHandler);
