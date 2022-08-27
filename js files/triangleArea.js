7e5bd0ca7ed6825340fe1d5e852d85b176abef67
Susmit Mukherjee <susmitmukherjee2001@gmail.com>
2022-08-27 20:48:45 +0530

    empty input case handled in triangleArea part

--- js files/triangleArea.js
+++ js files/triangleArea.js
@@ -4,16 +4,23 @@ const sideThree = document.querySelector("#sideThree");
 const btnCheck = document.querySelector("#btnCheck");
 
 function clickHandler() {
-    if (
-        parseInt(sideOne.value) + parseInt(sideTwo.value) > parseInt(sideThree.value) &&
-        parseInt(sideOne.value) + parseInt(sideThree.value) > parseInt(sideTwo.value) &&
-        parseInt(sideTwo.value) + parseInt(sideThree.value) > parseInt(sideOne.value)
-    ) {
-        var semiperi = (parseInt(sideOne.value) + parseInt(sideTwo.value) + parseInt(sideThree.value)) / 2
-        const area = Math.sqrt(semiperi * (semiperi - parseInt(sideOne.value)) * (semiperi - parseInt(sideTwo.value)) * (semiperi - parseInt(sideThree.value))).toFixed(4);
-        output.innerText = `Area of a triangle using heron's formula is ${area} units`;
-    } else {
-        output.innerText = "Enter valid side lengths such that each side lengths";
+    if(sideOne.value=="" || sideTwo.value=="" || sideThree.value==""){
+        output.innerText = "Invalid Inputs"
     }
+    else{
+        if (
+            parseInt(sideOne.value) + parseInt(sideTwo.value) > parseInt(sideThree.value) &&
+            parseInt(sideOne.value) + parseInt(sideThree.value) > parseInt(sideTwo.value) &&
+            parseInt(sideTwo.value) + parseInt(sideThree.value) > parseInt(sideOne.value)
+        ) {
+            var semiperi = (parseInt(sideOne.value) + parseInt(sideTwo.value) + parseInt(sideThree.value)) / 2
+            const area = Math.sqrt(semiperi * (semiperi - parseInt(sideOne.value)) * (semiperi - parseInt(sideTwo.value)) * (semiperi - parseInt(sideThree.value))).toFixed(4);
+            output.innerText = `Area of a triangle using heron's formula is ${area} units`;
+        } else {
+            output.innerText = "Enter valid side lengths such that each side lengths";
+        }
+
+    }
+   
 }
 btnCheck.addEventListener("click", clickHandler)
