d5959644020f501f803f6a5f51d131aa42cb2900
Susmit Mukherjee <susmitmukherjee2001@gmail.com>
2022-09-05 00:52:10 +0530

    error msg updated for triangle area

--- js files/triangleArea.js
+++ js files/triangleArea.js
@@ -5,11 +5,11 @@ const btnCheck = document.querySelector("#btnCheck");
 
 function clickHandler() {
     if(sideOne.value=="" || sideTwo.value=="" || sideThree.value==""){
-        output.innerText = "Enter all the fields"
+        output.innerText = "Enter all the fields";
     }
 
     else if(sideOne.value<0 || sideTwo.value<0 || sideThree.value<0){
-        output.innerText = "Enter Positive Values"
+        output.innerText = "Sides cannot be Negative";
     }
     else{
         if (
@@ -21,7 +21,7 @@ function clickHandler() {
             const area = Math.sqrt(semiperi * (semiperi - parseInt(sideOne.value)) * (semiperi - parseInt(sideTwo.value)) * (semiperi - parseInt(sideThree.value))).toFixed(4);
             output.innerText = `Area of a triangle using heron's formula is ${area} units`;
         } else {
-            output.innerText = "Enter valid side lengths such that each side lengths";
+            output.innerText = "Enter valid sides of a triangle";
         }
 
     }
