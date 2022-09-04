5f0c33667f50396481ed81a035c832ea576bba58
Susmit Mukherjee <susmitmukherjee2001@gmail.com>
2022-09-05 00:31:09 +0530

    negative input case handled in cal-hypotenuse

--- js files/Calculatehypotenuse.js
+++ js files/Calculatehypotenuse.js
@@ -5,10 +5,14 @@ const btnCheck = document.querySelector("#btnCheck");
 
 function clickHandler() {
     if(baseVal.value=="" || heightVal.value==""){
-        output.innerText = "Invalid Inputs"
+        output.innerText = "Enter all fields";
+    }
+
+   else if(baseVal.value < 0 || heightVal.value < 0){
+        output.innerText = "Enter Positive Values Only!!!";
     }
     else{
-        var result = Math.sqrt(parseInt(baseVal.value) * parseInt(baseVal.value) + parseInt(heightVal.value) * parseInt(heightVal.value));
+        var result = Math.sqrt(parseInt(baseVal.value) * parseInt(baseVal.value) + parseInt(heightVal.value) * parseInt(heightVal.value)).toFixed(2);
     output.innerText = "The length of hypotenuse is" + " " + result;
 
 
