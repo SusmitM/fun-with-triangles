603ee5aefb4b6d88b69f20604f3659f33f19c1e4
Susmit Mukherjee <susmitmukherjee2001@gmail.com>
2022-09-05 00:17:23 +0530

    negative angle case handled

--- js files/Angles of triangles.js
+++ js files/Angles of triangles.js
@@ -10,16 +10,23 @@ function sumCalculator() {
 }
 
 function clickHandler() {
+
     if(Angle1.value=="" || Angle2.value=="" || Angle3.value=="" ){
-        output.innerText = "Invalid Inputs"
+        output.innerText = "Invalid Inputs!!!";
     }
+    else if(Angle1.value<0 || Angle2.value<0 || Angle3.value<0 ){
+        output.innerText = "Enter Positive Values Only!!!";
+    }
+
     else{
         var sum = sumCalculator(parseInt(Angle1.value), parseInt(Angle2.value),parseInt(Angle3.value));
+
     if (sum === 180) {
         output.innerText = "Yay, the angles form a triangle!";
 
 
-    } else {
+    }
+     else {
         output.innerText = "Oh Oh! The angle doesn't form a triangle";
 
 
